a715056b96a4bcc83b35756f348ece5d25305bd3
DanielJoBar <jodar.barbero.daniel@gmail.com>
2024-01-25 10:34:56 +0100

    IncidentForm ya controla los datos que recibe +IncidentFormViewModel creado

---
+++ app/src/main/java/com/example/incidentmanager/ui/viewmodels/IncidentFormViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.incidentmanager.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class IncidentFormViewModel :ViewModel(){
+}
---
+++ app/src/main/java/com/example/incidentmanager/ui/viewmodels/IncidentViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.incidentmanager.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class IncidentViewModel :ViewModel(){
+
+}
