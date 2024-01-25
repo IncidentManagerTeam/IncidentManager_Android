db06742bcf690fc086f6b7933959000ee7f4eaa9
daluga0503 <daniel.luquegallardo@gmail.com>
2024-01-25 11:03:04 +0100

    navegación entre pantallas del MainACtivity

--- app/src/main/java/com/example/incidentmanager/MainActivity.kt
+++ app/src/main/java/com/example/incidentmanager/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.incidentmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.incidentmanager.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.incidentFragment,
+                R.id.parkingFragment,
+                R.id.userProfile
+            )
+        )
+        navView.setupWithNavController(navController)
     }
 }
