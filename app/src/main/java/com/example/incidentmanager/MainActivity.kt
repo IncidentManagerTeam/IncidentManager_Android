c211c5c4cb968008984884e58a18d6c7c6000361
DanielJoBar <jodar.barbero.daniel@gmail.com>
2024-03-04 09:48:06 +0100

    .

--- app/src/main/java/com/example/incidentmanager/MainActivity.kt
+++ app/src/main/java/com/example/incidentmanager/MainActivity.kt
@@ -18,5 +18,6 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentLoginMainArea) as NavHostFragment
         navController = navHostFragment.navController
+        //
     }
 }
