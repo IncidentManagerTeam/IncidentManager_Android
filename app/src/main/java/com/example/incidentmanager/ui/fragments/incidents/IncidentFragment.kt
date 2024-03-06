eb320476f4e8e3ff1e029e0cd58bdb07e67da85e
Alexgg420 <alexgg888999@gmail.com>
2024-03-06 19:39:55 +0100

    programa no explota

--- app/src/main/java/com/example/incidentmanager/ui/fragments/incidents/IncidentFragment.kt
+++ app/src/main/java/com/example/incidentmanager/ui/fragments/incidents/IncidentFragment.kt
@@ -1,44 +1,42 @@
 package com.example.incidentmanager.ui.fragments.incidents
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.viewModelScope
 import androidx.navigation.fragment.findNavController
-import com.example.incidentmanager.R
 import com.example.incidentmanager.databinding.FragmentIncidentBinding
-import com.example.incidentmanager.databinding.FragmentLoginBinding
-import com.example.incidentmanager.ui.fragments.user.LoginViewModel
-import com.example.incidentmanager.ui.viewmodels.IncidentViewModel
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
+import com.example.incidentmanager.ui.fragments.incidents.IncidentFragmentDirections
 
-@AndroidEntryPoint
 class IncidentFragment : Fragment() {
 
-    lateinit var binding: FragmentIncidentBinding;
-    private val viewModel:IncidentViewModel by viewModels();
+    private lateinit var binding: FragmentIncidentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentIncidentBinding.inflate(inflater,container,false)
+        // Inflar el diseño del fragmento y vincular la vista
+        binding = FragmentIncidentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.viewModelScope.launch {
-            binding.floatingActionButton.setOnClickListener{
-                var navigation = IncidentFragmentDirections.actionIncidentFragmentToFormIncidentFragment()
-                findNavController().navigate(navigation);
+
+        // Configurar el listener de clics para el botón flotante
+        binding.floatingActionButton.setOnClickListener {
+            // Navegar desde com.example.incidentmanager.ui.fragments.incidents.IncidentFragment a FormIncidentFragment
+            try {
+                val action = IncidentFragmentDirections.actionIncidentFragmentToFormIncidentFragment()
+                findNavController().navigate(action)
+            } catch (e: Exception) {
+                // Capturar y mostrar cualquier excepción que ocurra durante la navegación
+                Log.e("Navigation", "Error durante la navegación: ${e.message}")
             }
         }
     }
-
-    companion object { }
 }
